d395700716a6491b051de623532f671c2073cac7
hoehfurtnerk190028 <hoehfurtnerk190028@e33sws13>
2024-03-11 13:25:39 +0100

    Add project files.

---
+++ DataLayerLib/Examination.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayerLib
+{
+    public class Examination
+    {
+        public int ExaminationID {  get; set; }
+        public string Description { get; set; }
+
+        public int ExaminationPatientID { get; set; }
+        public Patient? ExaminationPatient { get; set; }
+        public int ExaminationDoctorID { get; set; }
+        public Doctor? ExaminationDoctor { get; set;}
+
+
+
+    }
+}
---
+++ DataLayerLib/HospitalDBContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayerLib
+{
+    internal class HospitalDBContext :DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Examination> Examinations { get; set; }
+
+        public HospitalDBContext()
+        {
+            
+        }
+
+        public HospitalDBContext(DbContextOptions<HospitalDBContext> options):base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            string conStr = @"server=(LocalDB)\mssqllocaldb;attachdbfilename=C:\Users\hoehfurtnerk190028\Downloads\DaataGrid-master\DaataGrid-master\DataLayerLib\HosDB.mdf; database=HosDBGrpA;integrated security=True;MultipleActiveResultSets=True;";
+
+            Console.WriteLine($"Using ConnStr={conStr}");
+            optionsBuilder.UseSqlServer(conStr);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Seed();
+        }
+    }
+}
