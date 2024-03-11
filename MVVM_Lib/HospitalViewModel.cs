c3360774676a884d90dd499309b4b594e3671b91
Klaus Hoehfurtner <HoehfurtnerK190028@sus.htl-grieskirchen.at>
2024-03-11 15:00:20 +0100

    comm

--- MVVM_Lib/HospitalViewModel.cs
+++ MVVM_Lib/HospitalViewModel.cs
@@ -3,48 +3,69 @@ using DataLayerLib;
 
 namespace MVVM_Lib
 {
-    public class HospitalViewModel: ObservableObject
+    public class HospitalViewModel : ObservableObject
     {
-        public HospitalDBContext?  DB{ get; set; }
+        public HospitalDBContext? DB { get; set; }
         public HospitalViewModel Init(HospitalDBContext newDB)
-		{
-		  DB = newDB;
-			Patients = DB.Patients.ToList();
-			Examinations = DB.Examinations.ToList();
-			return this;
-		}
-		private List<Patient> patients;
+        {
+            DB = newDB;
+            Patients = DB.Patients.ToList();
+            Examinations = DB.Examinations.ToList();
 
-		public List<Patient> Patients
-		{
-			get { return patients; }
-			set { patients = value;
-				RaisePropertyCHangedEvent(nameof(patients));
-			}
-		}
+            VMDoctors = DB.Doctors.Select(x => new DoctorViewModel { DoctorID = x.DoctorID, Firstname = x.Firstname, Lastname = x.Lastname, GPTDoctor =! x.Firstname.StartsWith("Z"),DoctorLink = new Uri("http://www.lusthouse.at/")}).ToList();
+            return this;
+        }
+        private List<Patient> patients;
 
-		private Patient? selectedPatient;
-
-		public Patient? SelectedPatient
-		{
-			get { return selectedPatient; }
-			set { selectedPatient = value;
-            RaisePropertyCHangedEvent(nameof(selectedPatient));
+        public List<Patient> Patients
+        {
+            get { return patients; }
+            set
+            {
+                patients = value;
+                RaisePropertyCHangedEvent(nameof(patients));
             }
-		}
+        }
 
-		private List<Examination> examinations;
+        private List<DoctorViewModel> _VMDoctors;
 
-		public List<Examination> Examinations
-		{
-			get { return examinations; }
-			set { examinations = value;
-				RaisePropertyCHangedEvent(nameof(examinations));
-			}
-		}
+        public List<DoctorViewModel> VMDoctors
+        {
+            get { return _VMDoctors; }
+            set
+            {
+                _VMDoctors = value;
+                RaisePropertyCHangedEvent(nameof(VMDoctors));
+            }
+        }
+
+
+        private Patient? selectedPatient;
+
+        public Patient? SelectedPatient
+        {
+            get { return selectedPatient; }
+            set
+            {
+                selectedPatient = value;
+                RaisePropertyCHangedEvent(nameof(selectedPatient));
+            }
+        }
+
+        private List<Examination> examinations;
+
+        public List<Examination> Examinations
+        {
+            get { return examinations; }
+            set
+            {
+                examinations = value;
+                RaisePropertyCHangedEvent(nameof(examinations));
+            }
+        }
 
 
 
-	}
+    }
 
 }
